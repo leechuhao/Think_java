895970f28729c53b8cb1f310e26d58c44791cb27
lichuhao <1010654308@qq.com>
2018-04-05 19:56:04 +0800

    Add files via upload
    2018/4/5

---
+++ RestaurantWithQueues.java
@@ -0,0 +1,30 @@
+//java编程思想
+//饭店仿真
+//页码：741~744
+class Order{
+
+}
+
+class Plate{
+
+}
+
+class Customer implements Runnable{
+
+}
+
+class waitPerson implements Runnable{
+
+}
+
+class Chef implements Runnable{
+
+}
+
+class Restaurant implements Runnable{
+
+}
+
+public class RestaurantWithQueues{
+	
+}
